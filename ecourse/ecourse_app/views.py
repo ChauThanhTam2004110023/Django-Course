3325c3b982c8e4d28c2ccacbc5ee511c2c50cb3b
ChauThanhTam2004110023 <90594941+ChauThanhTam2004110023@users.noreply.github.com>
2023-05-13 01:41:32 +0700

    nth-update

--- ecourse/ecourse_app/views.py
+++ ecourse/ecourse_app/views.py
@@ -14,20 +14,20 @@ from .forms import CreateUserForm
 
 class loginPage(View):
     def get(self, request):
-        self.page = "login"
-        if self.request.user.is_authenticated:
+        page = "login"
+        if request.user.is_authenticated:
             return redirect('home')
         context = {
-            'page': self.page
+            'page': page
         }
         return render(request, 'base/login_register.html', context)
     
     def post(self, request):
-        self.page = 'login'
-        if self.request.user.is_authenticated:
+        page = 'login'
+        if request.user.is_authenticated:
             return redirect('home')
-        username = self.request.POST.get('username')
-        password = self.request.POST.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
         except:
@@ -41,52 +41,29 @@ class loginPage(View):
         else:
             messages.error(request, 'Username or password does not exist.')
         context = {
-            'page': self.page
+            'page': page
         }
         return render(request, 'base/login_register.html', context)
 
-    
-# class registerPage(View):
-#     def get(self, request):
-#         self.form = UserCreationForm()
-#         context = {
-#             'form': self.form
-#         }
-#         return render(request, 'base/login_register.html', context)
-    
-#     def post(self, request):
-#         self.form = UserCreationForm(self.request.POST)
-#         if self.form.is_valid():
-#             self.user = self.form.save(commit=False)
-#             self.user.username = self.user.username
-#             self.user.save()
-#             login(request, self.user)
-#             return redirect('home')
-#         else:
-#             messages.error(request, 'An error occurred during registration?')
-#         context = {
-#             'form': self.form
-#         }
-#         return render(request, 'base/login_register.html', context)
 
 
 class registerPage(View):
     def get(self, request):
-        self.form = CreateUserForm()
+        form = CreateUserForm()
         context = {
-            'form': self.form
+            'form': form
         }
         return render(request, 'base/created_user.html', context)
     
     def post(self, request):
-        self.form = CreateUserForm(self.request.POST)
-        if self.form.is_valid():
-            self.form.save()
+        form = CreateUserForm(self.request.POST)
+        if form.is_valid():
+            form.save()
             return redirect('home')
         else:
             messages.error(request, 'An error occurred during registration?')
         context = {
-            'form': self.form
+            'form': form
         }
         return render(request, 'base/created_user.html', context)
 
@@ -99,51 +76,51 @@ class logoutPage(View):
 
 class home(View):
     def get(self, request):
-        self.q = self.request.GET.get('q') if self.request.GET.get('q') != None else ''
-        self.rooms = Room.objects.filter(
-            Q(topic__name__contains=self.q) |
-            Q(name__icontains=self.q) |
-            Q(descriptions__icontains=self.q))
+        q = request.GET.get('q') if request.GET.get('q') != None else ''
+        rooms = Room.objects.filter(
+            Q(topic__name__contains=q) |
+            Q(name__icontains=q) |
+            Q(descriptions__icontains=q))
 
-        self.topics = Topic.objects.all()
-        self.room_count = self.rooms.count()
-        self.room_messages = Message.objects.filter(Q(room__topic__name__icontains=self.q))
+        topics = Topic.objects.all()
+        room_count = rooms.count()
+        room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
         context = {
-            'rooms': self.rooms,
-            'topics': self.topics,
-            'room_count': self.room_count,
-            'room_messages': self.room_messages,
+            'rooms': rooms,
+            'topics': topics,
+            'room_count': room_count,
+            'room_messages': room_messages,
         }
         return render(request, 'base/home.html', context)
 
 
 class room(View):
     def get(self, request, pk):
-        self.room = Room.objects.get(id=pk)
-        self.room_messages = self.room.message_set.all()
-        self.participants = self.room.participants.all()
+        room = Room.objects.get(id=pk)
+        room_messages = room.message_set.all()
+        participants = room.participants.all()
 
         context = {
-            'room': self.room,
-            'room_messages': self.room_messages,
-            'participants': self.participants
+            'room': room,
+            'room_messages': room_messages,
+            'participants': participants
         }
         return render(request, 'base/room.html', context)
     
     def post(self, request, pk):
-        self.room = Room.objects.get(id=pk)
-        self.room_messages = self.room.message_set.all()
-        self.participants = self.room.participants.all()
+        room = Room.objects.get(id=pk)
+        room_messages = room.message_set.all()
+        participants = room.participants.all()
         
-        self.message = Message.objects.create(
-            user = self.request.user,
-            room = self.room,
-            body = self.request.POST.get('body')
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
         )
         context = {
-            'room': self.room,
-            'room_messages': self.room_messages,
-            'participants': self.participants
+            'room': room,
+            'room_messages': room_messages,
+            'participants': participants
         }
 
         return render(request, 'base/room.html', context)
@@ -152,20 +129,20 @@ class room(View):
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class createRoom(View):
     def get(self, request):
-        self.form = RoomForm()
+        form = RoomForm()
         context = {
-            "form": self.form
+            "form": form
         }
         return render(request, 'base/room_form.html', context)
 
     def post(self, request):
-        self.form = RoomForm(self.request.POST)
-        if self.form.is_valid():
-            self.room = self.form.save(commit=False)
-            self.host = self.request.user
-            self.room.save()
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            room = form.save(commit=False)
+            host = request.user
+            room.save()
             return redirect("home")
-        context = {"form": self.form}
+        context = {"form": form}
         return render(request, 'base/room_form.html', context)
 
 
@@ -173,21 +150,21 @@ class createRoom(View):
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class updateRoom(View):
     def get(self, request, pk):
-        self.listing = Room.objects.get(id=pk)
-        self.form = RoomForm(instance=self.listing)
+        listing = Room.objects.get(id=pk)
+        form = RoomForm(instance=listing)
         context = {
-            "form": self.form
+            "form": form
         }
         return render(request, 'base/room_form.html', context)
     
     def post(self, request, pk):
-        self.listing = Room.objects.get(id=pk)
-        self.form = RoomForm(self.request.POST ,instance=self.listing)
-        if self.form.is_valid():
-            self.form.save()
+        listing = Room.objects.get(id=pk)
+        form = RoomForm(request.POST ,instance=listing)
+        if form.is_valid():
+            form.save()
             return redirect("home")
         context = {
-            "form": self.form
+            "form": form
         }
         return render(request, 'base/room_form.html', context)
 
@@ -195,42 +172,42 @@ class updateRoom(View):
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class deleteRoom(View):
     def get(self, request, pk):
-        self.room = Room.objects.get(id=pk)
-        return render(request, 'base/delete.html', {'obj':self.room})
+        room = Room.objects.get(id=pk)
+        return render(request, 'base/delete.html', {'obj':room})
     def post(self, request, pk):
-        self.room = Room.objects.get(id=pk)
-        if self.request.method == 'POST':
-            self.room.delete()
+        room = Room.objects.get(id=pk)
+        if request.method == 'POST':
+            room.delete()
             return redirect('home')
-        return render(request, 'base/delete.html', {'obj':self.room})
+        return render(request, 'base/delete.html', {'obj':room})
 
 
 @method_decorator(login_required(login_url='login'), name='dispatch')
 class deleteMessage(View):
     def get(self, request, pk):
-        self.message = Message.objects.get(id=pk)
-        return render(request, 'base/delete.html', {'obj':self.message})
+        message = Message.objects.get(id=pk)
+        return render(request, 'base/delete.html', {'obj':message})
     def post(self, request, pk):
-        self.message = Message.objects.get(id=pk)
-        if self.request.method == 'POST':
-            self.message.delete()
+        message = Message.objects.get(id=pk)
+        if request.method == 'POST':
+            message.delete()
             return redirect('home')
-        return render(request, 'base/delete.html', {'obj':self.message})
+        return render(request, 'base/delete.html', {'obj':message})
     
 
 
 class userProfile(View):
     def get(self, request, pk):
-        self.user = User.objects.get(id=pk)
-        self.rooms = self.user.room_set.all()
-        self.topics = Topic.objects.all()
-        self.room_messages = self.user.message_set.all()
+        user = User.objects.get(id=pk)
+        rooms = self.user.room_set.all()
+        topics = Topic.objects.all()
+        room_messages = self.user.message_set.all()
 
         context = {
-            'user': self.user,
-            'rooms': self.rooms,
-            'topics': self.topics,
-            'room_messages': self.room_messages
+            'user': user,
+            'rooms': rooms,
+            'topics': topics,
+            'room_messages': room_messages
         }
         return render(request, 'base/profile.html', context)
     
